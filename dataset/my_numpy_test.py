eaa3dd20ecc3f32844f4ef4cd06ab0116cfdd3f8
sjaqj2010 <sjaqj2010@naver.com>
2024-08-07 10:34:51 +0900

    Add files via upload

---
+++ dataset/my_numpy_test.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib.image import imread
+
+
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x)+b
+    if tmp <=0:
+        return 0
+    else:
+        return 1
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int32)
+
+def sigmoid(x):
+    return 1 / (1+np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+
+import sys, os
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize = False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_train.shape)
